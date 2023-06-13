5bc34074e0b0b1ae530df37be4a4162d3a4e40c7
Yousef Elmesalamy <115108581+yousefelmesalamy@users.noreply.github.com>
2023-06-14 00:30:39 +0300

    finished isa lets deploy reqs

--- booktor/settings.py
+++ booktor/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #apps
-        'authentications',
+    'authentications',
     'alzhimarTest',
     'bloodtest',
     'heartTest',
